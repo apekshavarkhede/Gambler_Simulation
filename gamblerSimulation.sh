6eb2a06f73d681ef71e69617d67db8e35fa6dc89
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-03-08 13:55:52 +0530

    Added: if user win increase win count else increase loss count

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulation:"
+stake=100;
+chances=0;
+winCount=0;
+lossCount=0;
+chances=0;
+
+function play()
+{
+	number=$(( RANDOM % 2 ))
+	((chances++))
+		if [ $number -eq 1 ]
+		then
+			((winCount++))
+		elif [ $number -eq 0 ]
+		then
+			((lossCount++))
+		fi;
+}
