1d429f4875e90cca5c03d76c5a78725f6a81c6c4
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-03-08 14:37:02 +0530

    Added: stop the game after winning or loosing 50% of Stake

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,23 +1,33 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulation:"
+read -p "How many times you want to play:" noOfTimes
 stake=100;
 chances=0;
-winCount=0;
-lossCount=0;
+winMoney=0;
+looseMoney=0;
 chances=0;
-
+halfOfStake=$(( $stake - $stake/2 ))
 function play()
 {
 	number=$(( RANDOM % 2 ))
 	((chances++))
 		if [ $number -eq 1 ]
 		then
-			((winCount++))
+			((winMoney++))
 			((stake++))
 		elif [ $number -eq 0 ]
 		then
-			((lossCount++))
+			((looseMoney++))
 			((stake--))
 		fi;
 }
+
+for (( count=1; count<=$noOfTimes; count++ ))
+do
+if [[ $winMoney -eq $halfOfStake || $looseMoney -eq $halfOfStake ]]
+then
+	break
+fi
 play
+done
+
