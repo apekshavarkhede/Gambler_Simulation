f05a110346c6e9d46c503d082d686c8091fac720
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-03-09 12:33:11 +0530

    Added: display amount won and amount lost for each day

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -x
 echo "Welcome To Gambler Simulation:"
-stake=100;
-halfOfStake=$(( $stake - $stake/2 ))
-fullStake=$(( $stake + $stake/2 ))
-winMoney=0;
-looseMoney=0;
-days=20;
+days=30;
+declare -a WinArray
+declare -a lostArray
 function play()
 {
 	number=$(( RANDOM % 2 ))
@@ -15,20 +12,30 @@ function play()
 			((stake++))
 		elif [ $number -eq 0 ]
 		then
-			((looseMoney++))
+			((lostMoney++))
 			((stake--))
 		fi;
 }
 
 for (( day=1; day<=$days; day++ ))
 do
+stake=100;
+halfOfStake=$(( $stake - $stake/2 ))
+fullStake=$(( $stake + $stake/2 ))
+winMoney=0;
+lostMoney=0;
 	while [[ $stake -lt $fullStake && $stake -gt $halfOfStake ]]
 	do
 		play
 	done
+ 		WinArray[$day]=$winMoney;
+		lostArray[$day]=$lostMoney
 done
 
-echo  "You Win:: Rs.$winMoney You Lost::Rs.$looseMoney"
+
+echo "you win:: ${WinArray[@]}"
+echo "you lost:: ${lostArray[@]}"
+
 
 
 
